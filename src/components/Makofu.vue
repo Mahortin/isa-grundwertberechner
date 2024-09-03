1cf2f70425b050954076d8bc1e6430d4816da5a2
Mahortin <nitramwolfrum@web.de>
2024-09-03 21:39:03 +0200

    store skills in groups

--- src/components/Makofu.vue
+++ src/components/Makofu.vue
@@ -17,19 +17,26 @@ const attributes = reactive([
   {key: 'MU',name: 'Mut', value: 8},
   {key: 'KL', name: 'Klugheit', value: 8},
   {key: 'IN', name: 'Intuition', value: 8},
-  {key: 'CH', name: 'Charisma', value: 8}
+  {key: 'CH', name: 'Charisma', value: 8},
+  {key: 'FF', name: 'Fingerfertigkeit', value: 8},
+  {key: 'GE', name: 'Gewandheit', value: 8},
+  {key: 'KO', name: 'Konstitution', value: 8},
+  {key: 'KK', name: 'Körperkraft', value: 8}
 ])
 
-const skills = reactive([
-  {key: 'verborgenesErkennen', name: 'Verborgenes Erkennen', mapIsaAttributes: ['MU', 'KL', 'IN'], value: 0},
-  {key: 'handel', name: 'Handel', mapIsaAttributes: ['KL', 'IN', 'CH'], value: 0}
+const skillGroups = reactive([
+  {key: 'koerper', name: 'Körper', skills: [
+    {key: 'akrobatik', name: 'Akrobatik', mapIsaAttributes: ['MU', 'GE', 'KK'], value: 0},
+    {key: 'verborgenesErkennen', name: 'Verborgenes Erkennen', mapIsaAttributes: ['MU', 'KL', 'IN'], value: 0}
+  ]},
+  {key: 'gesellschaft', name: 'Gesellschaft', skills: [
+    {key: 'einschuechtern', name: 'Einschüchtern', mapIsaAttributes: ['MU', 'CH', 'KK'], value: 0},
+    {key: 'handel', name: 'Handel', mapIsaAttributes: ['KL', 'IN', 'CH'], value: 0}
+  ]}
 ])
 
 function calcEverything(key){
   var isaAttribute = attributes.find((attribute) => attribute.key === key)
-  calcWeisheit()
-  calcVerborgenesErkennen()
-  calcAllSkills()
   calcReferencedSkills(key)
 }
 
@@ -39,11 +46,14 @@ function calcAllSkills() {
 }
 
 function calcReferencedSkills(attributeKey) {
-  skills
-  .filter(skill => skill.mapIsaAttributes.includes(attributeKey))
-  .forEach(skill => {
-    calcSkill(skill)
+  skillGroups.forEach(group => {
+    group.skills
+      .filter(skill => skill.mapIsaAttributes.includes(attributeKey))
+      .forEach(skill => {
+        calcSkill(skill)
+    })
   })
+
 }
 
 function calcSkill(skill) {
@@ -69,11 +79,14 @@ function getAttribute(key) {
     <input type="number" min="8" max="16" @change="calcEverything(attribute.key)" v-model.number="attribute.value" 
   </div>
 
-  <div v-for="skill in skills">
-    <label>{{skill.key}}</label>
-    <label>{{skill.name}}</label>
-    <label>{{skill.mapIsaAttributes}}</label>
-    <label>{{skill.value}}</label>
+  <div v-for="group in skillGroups">
+    <label>{{group.name}}</label>
+      <div v-for="skill in group.skills">
+        <label>{{skill.key}}</label>
+        <label>{{skill.name}}</label>
+        <label>{{skill.mapIsaAttributes}}</label>
+        <label>{{skill.value}}</label>
+      </div>
   </div>
 
 </template>
