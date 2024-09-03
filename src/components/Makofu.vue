c261551b03b3f4df46cd6f551a9819c17cccc274
Mahortin <nitramwolfrum@web.de>
2024-09-03 16:56:49 +0200

    dynamic calculation for a single talent

--- src/components/Makofu.vue
+++ src/components/Makofu.vue
@@ -3,24 +3,55 @@ defineProps<{
   msg: string
 }>()
 import { ref } from 'vue'
+import { reactive, computed } from 'vue'
 
 const mut = ref(8)
-const klugheit = ref(12)
+const klugheit = ref(8)
+const intuition = ref(8)
+
+const verborgenesErkennen = ref(Math.round((mut.value + klugheit.value + intuition.value) / 3))
+const weisheit = ref(klugheit.value - mut.value)
+
 
 function setMut(e, v = +e.target.value) {
   mut.value = v
-  klugheit.value = v * (9 / 5) + 32
+  calcEverything()
 }
 
 function setKlugheit(e, v = +e.target.value) {
   klugheit.value = v
-  mut.value = (v - 32) * (5 / 9)
+  calcEverything()
 }
+
+function setIntuition(e, v = +e.target.value) {
+  intuition.value = v
+  calcEverything()
+}
+
+
+function calcEverything(){
+  calcWeisheit()
+  calcVerborgenesErkennen()
+  publishedBooksMessage.value = hasPublishedBooks()
+}
+
+function calcWeisheit() {
+  weisheit.value = (klugheit.value - mut.value)
+}
+function calcVerborgenesErkennen() {
+  verborgenesErkennen.value = Math.round((mut.value + klugheit.value + intuition.value) / 3)
+}
+
 </script>
 
 <template>
-  <input type="number" :value="mut" @change="setMut"> Mut =
+  <input type="number" :value="mut" @change="setMut"> Mut 
   <input type="number" :value="klugheit" @change="setKlugheit"> Klugheit
+  <input type="number" :value="intuition" @change="setIntuition"> Intuition
+
+  <p>Weisheit: {{ weisheit }}</p>
+  <p>Verborgenes erkennen: {{ verborgenesErkennen }}</p>
+
 </template>
 
 <style scoped>
