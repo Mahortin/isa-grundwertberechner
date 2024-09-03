b1ed2b805b380a85c44dc36bbaba22357c6f0440
Mahortin <nitramwolfrum@web.de>
2024-09-03 20:44:49 +0200

    create attribute inputs dynamically, select correct value from array

--- src/components/Makofu.vue
+++ src/components/Makofu.vue
@@ -5,6 +5,7 @@ defineProps<{
 import { ref } from 'vue'
 import { reactive, computed } from 'vue'
 
+const works = ref(0)
 const mut = ref(8)
 const klugheit = ref(8)
 const intuition = ref(8)
@@ -12,7 +13,7 @@ const intuition = ref(8)
 const verborgenesErkennen = ref(Math.round((mut.value + klugheit.value + intuition.value) / 3))
 const weisheit = ref(klugheit.value - mut.value)
 
-const attributes = ref([
+const attributes = reactive([
   {name: 'Mut', shortName: 'MU', value: 8},
   {name: 'Klugheit', shortName: 'KL', value: 10},
   {name: 'Intuition', shortName: 'IN', value: 100}
@@ -25,6 +26,7 @@ function setMut(e, v = +e.target.value) {
 }
 
 function setKlugheit(e, v = +e.target.value) {
+  console.log(`setKlugheit triggered`);
   klugheit.value = v
   calcEverything()
 }
@@ -35,17 +37,27 @@ function setIntuition(e, v = +e.target.value) {
 }
 
 
-function calcEverything(){
-  calcWeisheit()
-  calcVerborgenesErkennen()
-  publishedBooksMessage.value = hasPublishedBooks()
+function calcEverything(shortName){
+  var isaAttribute = attributes.find((attribute) => attribute.shortName === shortName)
+  calcEverythingBackup()
+  var test = getAttribute(shortName)
 }
 
 function calcWeisheit() {
-  weisheit.value = (klugheit.value - mut.value)
+  var isaKL = attributes.find((attribute) => attribute.shortName === "KL")
+  var isaMU = attributes.find((attribute) => attribute.shortName === "MU")
+  weisheit.value = isaKL.value - isaMU.value
 }
+
 function calcVerborgenesErkennen() {
-  verborgenesErkennen.value = Math.round((mut.value + klugheit.value + intuition.value) / 3)
+  var isaMU = attributes.find((attribute) => attribute.shortName === "MU")
+  var isaKL = attributes.find((attribute) => attribute.shortName === "KL")
+  var isaIN = attributes.find((attribute) => attribute.shortName === "IN")
+  verborgenesErkennen.value = Math.round((isaMU.value + isaKL.value + isaIN.value) / 3)
+}
+
+function getAttribute(shortName) {
+  return attributes.find((attribute) => attribute.shortName === shortName)
 }
 
 </script>
@@ -55,15 +67,12 @@ function calcVerborgenesErkennen() {
     <label>{{attribute.name}}</label>
     <label>{{attribute.shortName}}</label>
     <label>{{attribute.value}}</label>
-    <input type="number" min="8" max="16" v-model="attribute.value" 
+    <input type="number" min="8" max="16" @change="calcEverything(attribute.shortName)" v-model.number="attribute.value" 
   </div>
-
-  <input type="number" min="8" max="16" :value="mut" @change="setMut"> Mut 
-  <input type="number" :value="klugheit" @change="setKlugheit"> Klugheit
-  <input type="number" :value="intuition" @change="setIntuition"> Intuition
 
   <p>Weisheit: {{ weisheit }}</p>
   <p>Verborgenes erkennen: {{ verborgenesErkennen }}</p>
+  <p>Works: {{ works }}</p>
 
 </template>
 
