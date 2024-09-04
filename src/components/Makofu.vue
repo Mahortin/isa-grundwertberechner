e2423821bfc8170c434289d78036df6dc9a0966c
Mahortin <nitramwolfrum@web.de>
2024-09-04 22:20:31 +0200

    nicer input fields

--- src/components/Makofu.vue
+++ src/components/Makofu.vue
@@ -136,7 +136,6 @@ function toggleFilter() {
         <div class="attribute-info">
           <span class="attribute-key">{{ attribute.key }}</span>
           <span class="attribute-name">{{ attribute.name }}</span>
-          <span class="attribute-value">{{ attribute.value }}</span>
           
         </div>
         <input 
@@ -163,8 +162,8 @@ function toggleFilter() {
         <div v-for="skill in group.skills" :key="skill.key" class="skill-item">
           <div v-if="!showOnlyIncreasedSkills || (showOnlyIncreasedSkills && skill.increased)" :class="[skill.increased ? 'skill-info-highlighted' : 'skill-info']">
             <span class="skill-name">{{ skill.name }}</span>
-            <span class="skill-attributes">{{ skill.mapIsaAttributes }}</span>
             <span class="skill-value">{{ skill.value }}</span>
+            <span class="skill-attributes">{{ skill.mapIsaAttributes }}</span>
           </div>
         </div>
       </div>
@@ -277,6 +276,27 @@ button.highlight-button:hover {
 
 h3, h4 {
   margin-bottom: 15px;
+}
+
+input {
+  background-color: #2c3e50; /* Darker background for modern look */
+  color: #ecf0f1; /* Light text for contrast */
+  border: 1px solid #34495e; /* Subtle, softer border */
+  padding: 10px 15px;
+  border-radius: 6px; /* Rounded corners */
+  font-size: 16px;
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1); /* Subtle inner shadow for depth */
+  transition: border-color 0.3s ease, box-shadow 0.3s ease; /* Smooth transitions */
+}
+
+input:focus {
+  border-color: #1abc9c; /* Slightly brighter border on focus */
+  box-shadow: 0 0 5px rgba(26, 188, 156, 0.5); /* Add a subtle glow effect on focus */
+  outline: none; /* Remove the default outline */
+}
+
+input::placeholder {
+  color: #7f8c8d; /* Slightly lighter placeholder text */
 }
 
 </style>
