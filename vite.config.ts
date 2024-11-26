1b8e6ef3758f9cea521e495b7b8d9468b92d1eb6
Mahortin <nitramwolfrum@web.de>
2024-11-26 16:48:53 +0100

    remove unused icon, update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/isa-grundwertberechner/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+    sourcemap: true // Optional: Helps debug deployment issues
   }
 })
