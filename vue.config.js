6a6652039341fd2a1a46ca40231d86520ffbfd2e
Mahortin <nitramwolfrum@web.de>
2025-02-12 15:35:21 +0100

    try to deploy github page

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/isa-grundwertberechner/' : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig(
+  (module.exports = {
+    publicPath: '/ischeroth-werterechner/'
+  })
+)
